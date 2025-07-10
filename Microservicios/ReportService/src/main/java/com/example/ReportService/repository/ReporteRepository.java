be67e4dab2d0412e5906fd1e539bdc70ee3ca252
Boychill <168212086+Boychill@users.noreply.github.com>
2025-07-09 23:16:31 -0400

    Delete Microservicios/ReportService directory

--- Microservicios/ReportService/src/main/java/com/example/ReportService/repository/ReporteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ReportService.repository;
-
-import com.example.ReportService.model.Reporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReporteRepository extends JpaRepository<Reporte, Long> {
-}
